d0bbd6f16b627c32144b3d96a0cc19b4121da48b
Frdman <akazyamova@outlook.com>
2024-02-27 02:59:04 +0400

    done task

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ var submitButton = document.getElementById('submitButton');
 var outputContainer = document.getElementById('output');
 
 // Добавляем обработчик события на кнопку "Начать"
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
     popupContainer.style.display = 'block';
-    startButton.style.display = 'none';
 });
 
 // Добавляем обработчик события на кнопку "Отправить"
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', function () {
     // Получаем данные из полей ввода
     var inputs = document.querySelectorAll('.popup input[type="text"]');
     var inputData = {};
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         inputData[input.placeholder] = input.value;
     });
 
@@ -23,11 +22,11 @@ submitButton.addEventListener('click', function() {
     // Создаем сохранение данных в localStorage
     localStorage.setItem('inputData', JSON.stringify(inputData));
 
-     // Закрываем попап
-     popupContainer.style.display = 'none';
+    // Закрываем попап
+    popupContainer.style.display = 'none';
 
-     //Выведем данные на страницу
-     var outputHTML = '<h2>Введенные данные:</h2><ul>';
+    //Выведем данные на страницу
+    var outputHTML = '<h2>Данные анкеты:</h2><ul>';
     for (var key in inputData) {
         outputHTML += '<li>' + key + ' ' + inputData[key] + '</li>';
     }
@@ -35,7 +34,7 @@ submitButton.addEventListener('click', function() {
     outputContainer.innerHTML = outputHTML;
 });
 
-window.addEventListener('click', function(event) {
+window.addEventListener('click', function (event) {
     if (event.target == popupContainer) {
         popupContainer.style.display = 'none';
     }
